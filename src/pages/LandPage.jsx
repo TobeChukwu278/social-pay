eb0af2e319704df18ab33812519f19c1fdc8a40c
Tøbé Çhükwû <astrolix278@gmail.com>
2025-06-06 12:43:27 +0100

    modified the landpage component file

--- src/pages/LandPage.jsx
+++ src/pages/LandPage.jsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import NavBar from '../components/NavBar'
 import Home from '../components/Home'
+import About from '../components/About'
+import Objectives from '../components/Objectives'
+import ToolSlider from '../components/ToolSlider'
+import Faq from '../components/Faq'
+import Footer from '../components/Footer'
 
 const LandPage = () => {
     return (
         <div>
             <NavBar />
             <Home />
+            <About />
+            <Objectives />
+            <ToolSlider />
+            <Faq />
+            <Footer />
         </div>
     )
 }
